27b82a246ba1dc57d16af3462a4c153180f8cc8d
ProjectOmega369 <luca.up@yahoo.com>
2024-11-27 22:40:10 -0300

    modificações do tooltip

--- src/routes/graficos/+page.svelte
+++ src/routes/graficos/+page.svelte
@@ -1,19 +1,87 @@
 <script lang="ts">
     import type { AllChartsType } from "$lib/types/charts/AllChartsType";
     import { Line, Pie, Doughnut, Bar } from 'svelte-chartjs'
-    import { Chart as ChartJS, registerables } from 'chart.js'
+    import { Chart as ChartJS, registerables} from 'chart.js'
+    import type {TooltipItem} from 'chart.js'
+    import { text } from "@sveltejs/kit";
+    import { Weight } from "lucide-svelte";
     ChartJS.register(...registerables)
     export let data: AllChartsType;
 
     if (data) {
         console.log(data);
     }
+
+
+    const config = {
+        maintainAspectRatio: false, // Permite ajustar a altura e largura do gráfico
+        responsive: true, // Torna o gráfico responsivo
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (context:TooltipItem <'bar'>) {
+                        const datasetIndex = context.datasetIndex || '';
+                        const index = context.dataIndex;
+
+                        const label = context.dataset.label;
+
+                        const labelText = Array.isArray(label) ? label[index] : label; 
+
+                        // Se o label não existir ou estiver vazio, usamos um fallback
+                    const displayLabel = labelText ? labelText : 'Descrição Não Encontrada';
+
+                        const value = context.raw;
+                        return `${displayLabel}: ${value}`;
+                    }
+                },
+                padding: 20,
+                bodyFont: {
+                    size: 14,
+                    weight: 700
+                },
+                boxPadding: 10,
+                displayColors: false
+            },
+            title: {
+                display: true,
+                text: 'Titulo do Grafico',
+                font: {
+                    size: 24,
+                    Weight: 'bold',
+                    family: 'Arial',
+                },
+                padding: {
+                    top: 10,
+                    bottom: 10,
+                    left: 20,
+                    right: 20
+                }
+            }
+        },
+        layout: {
+            padding: {
+                top: 10,
+                bottom: 10,
+                left: 20,
+                right: 20
+            }
+        },
+        scales: {
+            x: {
+                beginAtZero: true
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
 </script>
 
 <main class="grid-charts-container mx-auto max-w-4xl gap-4 mb-16">
     {#if data.enquadramento}
         <div class="graph-one w-full p-4 rounded-lg bg-c-secondary">
-            <Bar data={data.enquadramento} width={300} height={300} options={{ maintainAspectRatio: false }} />
+            <Bar data={data.enquadramento} width={300} height={300} options={config} />
         </div>
     {/if}
     {#if data.natureza}
@@ -23,7 +91,7 @@
     {/if}
     {#if data.marca_veiculo}
         <div class="graph-three w-full p-4 rounded-lg bg-c-secondary">
-            <Bar data={data.marca_veiculo} width={300} height={300} options={{ maintainAspectRatio: false }} />
+            <Bar data={data.marca_veiculo} width={300} height={300} options={config} />
         </div>
     {/if}
     {#if data.velocidade_regulamentada}
